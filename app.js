5ff84e414244fe3dbac3043b85b70d95f1c27ccb
anthony-baru <anthonybaru@gmail.com>
2020-05-13 08:39:23 +0300

    adding mongoose

--- app.js
+++ app.js
@@ -2,20 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.port || 8080;
+const cors = require('cors');
+const mongoose = require('mongoose');
 //body parser
 app.use(bodyParser.json());
-
+app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Header', 'Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization');
     next();
 })
 
 // routes
 const feedRoutes = require('./routes/feed');
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.json({ 'msg': 'Hello World!' }))
 
 app.use('/feed', feedRoutes);
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+mongoose
+    .connect('mongodb://localhost:27017/messages?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(result => {
+        app.listen(port, () => console.log(`app listening on port ${port}!`))
+    })
+    .catch(err => { console.log(err) })
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,40 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({ posts: 25 })
+    res.status(200).json({
+        posts: [{
+            _id: '1',
+            title: 'First Post',
+            content: 'This is the first post!',
+            imageUrl: 'images/Game of Thrones TV Series Wallpapers Set-13 [Spy1984] (8).jpg',
+            creator: {
+                name: 'Tony B'
+            },
+            createdAt: new Date()
+        }]
+    })
 }
 
-exports.postPost = (req, res, next) => {
-    console.log(req.body);
+exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json({ message: 'Validation failed, Incorrect data entered!', errors: errors.array() })
+    }
     const title = req.body.title;
     const content = req.body.content;
     //create post in db
     res.status(200).json({
         message: 'Post created successfully!',
         post: {
-            id: new Date().toISOString(),
+            _id: new Date().toISOString(),
             title: title,
-            content: content
+            content: content,
+            creator: {
+                name: 'Tony B'
+            },
+            createdAt: new Date()
         }
     })
 }
