c91526a5ad3822d45e710301a57ab768dce509e3
anthony-baru <anthonybaru@gmail.com>
2020-05-13 18:41:18 +0300

    fixed uploading issues

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const fs = require('fs');
 const Post = require('../models/post');
 exports.getPosts = (req, res, next) => {
     Post
@@ -75,4 +76,56 @@ exports.getPost = (req, res, next) => {
             }
             next(err);
         })
+}
+
+exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, Incorrect data entered!');
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+        imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+        const error = new Error('No file picked.')
+        error.statusCode = 400;
+        throw error;
+    }
+
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                if (!post) {
+                    const error = new Error(`Couldn't find post!`);
+                    error.statusCode = 404;
+                    throw error;
+                }
+            }
+            post.title = title;
+            post.content = content;
+            post.imageUrl = imageUrl;
+            return post.save();
+        })
+        .then(result => {
+            result.status(200).json({
+                message: `${title} updated successfully!`,
+                statusCode: 200
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+
+}
+
+const clearImage = (req, res, next) => {
+
 }
